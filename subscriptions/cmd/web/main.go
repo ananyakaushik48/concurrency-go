1fd68760edd2990496538846f154109a540528b4
ananya <ananyakaushik48@gmail.com>
2024-07-13 13:41:44 +0530

    removed the db-data from the commit and initialising the subscription service

---
+++ subscriptions/cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to Database
+	
+	// create sessions
+
+	// create channels
+
+	// wait group 
+
+	// setup the application config
+
+	// setup mail
+
+	// setup wait
+
+	// listen for web connections
+}
